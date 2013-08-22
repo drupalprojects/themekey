edb26c0e2f76cc8e106c468a6152df9549c3c26e
Markus Kalkbrenner <markus.kalkbrenner@bio.logis.de>
2013-08-22 19:19:23 +0200

    added a block to allow manually switching back to previous or different URL after redirect

--- themekey_redirect/themekey_redirect.js
+++ themekey_redirect/themekey_redirect.js
@@ -1,13 +1,20 @@
-(function ($) {
-    $.ajax({
-        // add the current path and query to the url for ThemeKey's rule matching
-        url: '/themekey/redirect_callback' + window.location.pathname + window.location.search,
-        dataType: 'json',
-        type: 'GET',
-        success: function(target) {
-            if (target) {
-                window.location.href = target;
+jQuery(document).ready(function($) {
+    var state = $.cookie('themekey_redirect_state');
+    if (state != 1 && state != 2) {
+        $.ajax({
+            // add the current path and query to the url for ThemeKey's rule matching
+            url: '/themekey/redirect_callback' + window.location.pathname + window.location.search,
+            dataType: 'json',
+            type: 'GET',
+            success: function(target) {
+                if (target) {
+                    window.location.href = target;
+                }
             }
-        }
-    });
-})(jQuery);
+        });
+    }
+    else if (state == 1) {
+        $('#block-themekey-redirect-domain-selector').show();
+        $.cookie('themekey_redirect_state', 2, { path: '/'});
+    }
+});
